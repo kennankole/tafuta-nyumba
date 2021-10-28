e3f4084421ca21465673bae949953f4313cc28c8
kennankole <kennankole@gmail.com>
2021-10-28 12:18:23 +0300

    Dockerizing the application

--- app/business/utils.py
+++
@@ -1,3 +0,0 @@
-
-def get_type_of_business_premises(biz_id, types_of_business_premises):
-    return types_of_business_premises.get(biz_id)
--- app/houses/utils.py
+++
@@ -1,2 +0,0 @@
-def get_type_of_house(id, type_of_houses):
-    return type_of_houses.get(id)
