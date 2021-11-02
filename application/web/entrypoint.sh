1966e5cf274a454ef2ff6c66f68b1a306f262f58
kennankole <kennankole@gmail.com>
2021-11-02 13:00:22 +0300

    Deploying to heroku

--- application/web/entrypoint.sh
+++ application/web/entrypoint.sh
@@ -1,22 +1,24 @@
 #!/bin/sh
-if [ "$DATABASE" = "postgres" ]
-then 
-    echo "Waiting for postgres..."
+# if [ "$DATABASE" = "postgres" ]
+# then 
+#     echo "Waiting for postgres..."
 
-    while ! nc -z "$SQL_HOST" "$SQL_PORT"; do
-        sleep 0.1 
-    done
+#     while ! nc -z "$SQL_HOST" "$SQL_PORT"; do
+#         sleep 0.1 
+#     done
 
-    echo "PostgreSQL started"
+#     echo "PostgreSQL started"
 
-fi
+# fi
 
-if [ "$FLASK_ENV" = "development" ]
-then 
-    echo "Creating the database tables..."
-    python manage.py create_db
-    echo "Tables created"
+# if [ "$FLASK_ENV" = "development" ]
+# then 
+#     echo "Creating the database tables..."
+#     python manage.py create_db
+#     echo "Tables created"
 
-fi
+# fi
 
-exec "$@"
+# exec "$@"
+gunicorn --bind 0.0.0.0:5000 manage:app
+# gunicorn services.web.manage:app -w 2 --threads 2 -b 0.0.0.0:"$PORT"
