bc3cfb29e7f4b09dec958881c716e65b09fd4dd5
kennankole <kennankole@gmail.com>
2021-11-03 09:25:50 +0300

    Deploying to heroku

--- application/web/app/views.py
+++ application/web/app/views.py
@@ -20,7 +20,7 @@ from app.menu.menu import LowerLevelMenu
 views = Blueprint("views", __name__)
 
 
-@views.route("/")
+@views.route("/", methods=["POST", "GET"])
 def home_page():
     return "Welcome to Tafuta Nyumba"
 
