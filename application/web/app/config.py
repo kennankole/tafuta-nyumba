edfd1df4903ad6effa6a973830128e459b9b54f4
kennankole <kennankole@gmail.com>
2021-11-03 12:15:06 +0300

    Fixing environmental variables

--- application/web/app/config.py
+++ application/web/app/config.py
@@ -1,14 +1,12 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
+from decouple import config
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite://")
+    SQLALCHEMY_DATABASE_URI = config("DATABASE_URL", "sqlite://")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv("SECRET_KEY") or "AS SECRET KEY"
-    APP_NAME = os.getenv("APP_NAME")
-    STATIC_FOLDER = f"{os.getenv('APP_FOLDER')}/project/static"
-    MEDIA_FOLDER = f"{os.getenv('APP_FOLDER')}/project/media"
-    USERNAME = os.getenv("USERNAME")
-    API_KEY = os.getenv("API_KEY")
+    SECRET_KEY = config("SECRET_KEY") or "AS SECRET KEY"
+    APP_NAME = config("APP_NAME")
+    STATIC_FOLDER = f"{config('APP_FOLDER')}/project/static"
+    MEDIA_FOLDER = f"{config('APP_FOLDER')}/project/media"
+    USERNAME = config("USERNAME")
+    API_KEY = config("API_KEY")
