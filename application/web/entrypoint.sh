737d5e564060d8bf2baa646cfc62032a09a09dc5
kennankole <kennankole@gmail.com>
2021-11-11 07:42:23 +0300

    Fixing entrypoint command

--- application/web/entrypoint.sh
+++ application/web/entrypoint.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
-if [ "$DATABASE" = "postgres" ]
-then 
-    echo "Waiting for postgres..."
+# if [ "$DATABASE" = "postgres" ]
+# then 
+#     echo "Waiting for postgres..."
 
-    while ! nc -z "$SQL_HOST" "$SQL_PORT"; do
-        sleep 0.1 
-    done
+#     while ! nc -z "$SQL_HOST" "$SQL_PORT"; do
+#         sleep 0.1 
+#     done
 
-    echo "PostgreSQL started"
+#     echo "PostgreSQL started"
 
-fi
+# fi
 
-if [ "$FLASK_ENV" = "development" ]
-then 
-    echo "Creating the database tables..."
-    python manage.py create_db
-    echo "Tables created"
+# if [ "$FLASK_ENV" = "development" ]
+# then 
+#     echo "Creating the database tables..."
+#     python manage.py create_db
+#     echo "Tables created"
 
-fi
+# fi
 
-exec "$@"
+# exec "$@"
 
 # gunicorn --bind 0.0.0.0:5000 application.web.app.manage:app
 # gunicorn app.manage:app -w 2 --threads 2 -b 0.0.0.0:"$PORT"
 
-# gunicorn --bind 0.0.0.0:"$PORT" manage:app # Deployed to heroku
+gunicorn --bind 0.0.0.0:"$PORT" manage:app # Deployed to heroku
