7c8235fdd5f4109195c45a25d7abb2eb0cda7e14
kennankole <kennankole@gmail.com>
2021-11-14 12:22:00 +0300

    Fixing app name from envar

--- application/web/app/menu/base_menu.py
+++ application/web/app/menu/base_menu.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 from flask import current_app, make_response
 from decouple import config
@@ -31,7 +32,7 @@ class Menu:
         return response
 
     def home_menu(self):
-        menu_text = f"Welcome to {config('APP_NAME')}\n Choose a service\n"
+        menu_text = f"Welcome to {os.environ.get('APP_NAME')}\n Choose a service\n"
         menu_text += "1. Houses\n"
         menu_text += "2. Hostels\n"
         menu_text += "3. Business Premises \n"
