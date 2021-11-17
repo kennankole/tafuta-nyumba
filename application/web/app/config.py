19f0e1c8865b5ab11033c3b47fcd460bb8c715eb
kennankole <kennankole@gmail.com>
2021-11-17 14:39:22 +0300

    Modifying config.py file

--- application/web/app/config.py
+++ application/web/app/config.py
@@ -6,7 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///real_estate.db")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///real_estate.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY") or "AS SECRET KEY"
     DATABASE = os.getenv("DATABASE")
