abfec05e3052ebe7146abe9ce3c10e5852a85de8
kennankole <kennankole@gmail.com>
2021-11-18 12:36:53 +0300

    fixing envar for heroku postgres

--- application/web/app/config.py
+++ application/web/app/config.py
@@ -1,14 +1,14 @@
 import os
-import re
+
 from decouple import config
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-uri = config("DATABASE_URL")
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = uri
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    if SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
+        SQLALCHEMY_DATABASE_URI.replace('postgres://', 'postgresql://', 1) 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY") or "AS SECRET KEY"
     DATABASE = os.getenv("DATABASE")
