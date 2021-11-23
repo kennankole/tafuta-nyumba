7b970609c01a3d0502bddea2af82a97d20d82c19
kennankole <kennankole@gmail.com>
2021-11-23 13:27:33 +0300

    fixing DATABASE_URL for Heroku Postgres

--- application/web/app/config.py
+++ application/web/app/config.py
@@ -24,6 +24,10 @@ class Config:
     REDIS_URL = config("REDIS_URL", default="localhost")
     REDIS_DB = os.environ.get("REDIS_DB")
     REDISTOGO_URL = os.getenv("REDISTOGO_URL", default="localhost")
+    
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+      uri =  SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+    SQLALCHEMY_DATABASE_URI = uri
 
    
 class TestConfig(Config):
