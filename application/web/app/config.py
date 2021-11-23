813b4ef1c48580a4f3db51e78740803b239019a5
kennankole <kennankole@gmail.com>
2021-11-23 13:56:11 +0300

    fixing DATABASE_URL for Heroku Postgres

--- application/web/app/config.py
+++ application/web/app/config.py
@@ -7,7 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 
-uri = "postgres://voavcpovmoccjx:d9e5d1d6bae8f9eeed4c61a052b1770fa50845646a016f67250daa397b09f5b4@ec2-34-204-226-228.compute-1.amazonaws.com:5432/dd9430bj1tc15v"
+uri = "postgresql://voavcpovmoccjx:d9e5d1d6bae8f9eeed4c61a052b1770fa50845646a016f67250daa397b09f5b4@ec2-34-204-226-228.compute-1.amazonaws.com:5432/dd9430bj1tc15v"
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY") or "AS SECRET KEY"
