91a5d82fc523b5daae2e7c8552471df883264792
kennankole <kennankole@gmail.com>
2021-11-23 13:50:45 +0300

    fixing DATABASE_URL for Heroku Postgres

--- application/web/app/config.py
+++ application/web/app/config.py
@@ -7,9 +7,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 
-
+uri = "postgres://voavcpovmoccjx:d9e5d1d6bae8f9eeed4c61a052b1770fa50845646a016f67250daa397b09f5b4@ec2-34-204-226-228.compute-1.amazonaws.com:5432/dd9430bj1tc15v"
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:nyumba@localhost/nyumba_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY") or "AS SECRET KEY"
     DATABASE = os.getenv("DATABASE")
@@ -24,7 +23,6 @@ class Config:
     REDIS_URL = config("REDIS_URL", default="localhost")
     REDIS_DB = os.environ.get("REDIS_DB")
     REDISTOGO_URL = os.getenv("REDISTOGO_URL", default="localhost")
-    uri = SQLALCHEMY_DATABASE_URI
     if uri and uri.startswith("postgres://"):
         uri.replace("postgres://", "postgresql://", 1)
     SQLALCHEMY_DATABASE_URI = uri
