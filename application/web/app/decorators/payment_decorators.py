765e730cf69ba48f272421beebdfa760aa14f2ed
kennankole <kennankole@gmail.com>
2021-11-30 11:36:07 +0300

    Implementing STK push M-Pesa payment

--- application/web/app/decorators/payment_decorators.py
+++ application/web/app/decorators/payment_decorators.py
@@ -1,17 +1,22 @@
+import africastalking
+from africastalking import Payment
+from sqlalchemy.ext.declarative import api
+from app.config import Config
 from app.models import CapturingUserData
-
 
 def charge_users_decorator(func):
     def charge_users_innermost_decorator(self, *args, **kwargs):
+        
         charge_users_innermost_decorator.calls += 1
         if charge_users_innermost_decorator.calls >= 3:
-            # if (
-            #     CapturingUserData.query.filter_by(contacts=self.phone_number).first()
-            #     is not None
-            # ):
+            user_name = "sandbox"
+            api_key = Config.API_KEY
+            africastalking.initialize(user_name, api_key)
+            payment = africastalking.Payment
             menu_text = "Kindly pay up to continue enjoying our services\n"
+            payment.mobile_checkout("Tafuta-Nyumba", self.phone_number, "KES", 20)
             menu_text += "We are sending you an M-Pesa checkout of KES: 20\n"
-            return self.ussd_continue(menu_text)
+            return self.ussd_end(menu_text)
         else:
             return func(self, *args, **kwargs)
 
